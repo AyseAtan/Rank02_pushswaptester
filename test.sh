5951f78a462e2ee5a0cee667ea134740211511dc
Ayse Atan <aatan@c4c5c6.42berlin.de>
2024-02-19 17:42:51 +0100

    Test05

--- test.sh
+++ test.sh
@@ -12,8 +12,8 @@ ARG="4 67 3 87 23"; ./push_swap $ARG | ./test/checker_linux $ARG
 echo " >> with: ./push_swap 2 1 3 6 5 8"
 ./push_swap 2 1 3 6 5 8
 
-echo " >> with invalid input: ./push_swap 0 one 2 3"
-./push_swap 0 one 2 3
+echo " >> with invalid input: ./push_swap 0 one 2 3 |wc -l"
+./push_swap 0 one 2 3 |wc -l
 
 echo " >> with invalid input: ./push_swap 0 2 2 3"
 ./push_swap 0 2 2 3 
