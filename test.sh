df7dab87aad2f4c53ed16e6c05ff889ca3ff5eca
Ayse Atan <aatan@c4c5c6.42berlin.de>
2024-02-19 16:58:23 +0100

    Test04

--- test.sh
+++ test.sh
@@ -3,11 +3,11 @@
 echo " >> Suffle 0 to 100000"
 ./push_swap $(shuf -i 0-100000 -n 500) |wc -l
 
-echo " >> Shuffle 0 to 100000 with 42 ./checker"
-ARG="4 67 3 87 23"; ./push_swap $ARG | ./test/checker_linux $ARG
-
 echo " >> with: ARG="4 67 3 87 23"; ./push_swap $ARG | wc -l"
 ARG="4 67 3 87 23"; ./push_swap $ARG | wc -l
+
+echo " >> with ARG="4 67 3 87 23"; ./push_swap $ARG | ./test/checker_linux $ARG"
+ARG="4 67 3 87 23"; ./push_swap $ARG | ./test/checker_linux $ARG
 
 echo " >> with: ./push_swap 2 1 3 6 5 8"
 ./push_swap 2 1 3 6 5 8
