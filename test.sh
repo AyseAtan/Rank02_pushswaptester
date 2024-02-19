1220be8d91654db65241ff2f5692e2a2bb29d5a7
Ayse Atan <aatan@c4c5c6.42berlin.de>
2024-02-19 16:27:13 +0100

    Test03

--- test.sh
+++ test.sh
@@ -1,7 +1,12 @@
-./push_swap $(shuf -i 0-100000 -n 500) |wc -l
-./push_swap $(shuf -i 0-100000 -n 500) | .checker_linux $(shuf -i 0-100000 -n 500)
-ARG="4 67 3 87 23"; ./push_swap $ARG | wc -l
-./push_swap "4 67 3 87 23"
+#!/bin/bash
+
+echo "Suffle 0 to 100000"
+.././push_swap $(shuf -i 0-100000 -n 500) |wc -l
+echo "Shuffle with 42 ./checker"
+.././push_swap $(shuf -i 0-100000 -n 500) | ./checker_linux $(shuf -i 0-100000 -n 500)
+echo "with ARGs 4 67 3 87 23"
+ARG="4 67 3 87 23"; .././push_swap $ARG | wc -l
 ./push_swap 2 1 3 6 5 8
+echo "with invalid input"
 ./push_swap 0 one 2 3
 ./push_swap 0 2 2 3 
