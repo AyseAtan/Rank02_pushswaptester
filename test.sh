805b5ac09818237d41f9b65138a832e2a7e8fab0
Ayse Atan <aatan@c4c5c6.42berlin.de>
2024-02-19 18:22:38 +0100

    Test

--- test.sh
+++ test.sh
@@ -7,29 +7,28 @@ if [ "$#" -ne 5 ]; then
 fi
 
 # Define input files for each set of commands
-test1.txt="$1"
-test2.txt="$2"
-test3.txt="$3"
-test4.txt="$4"
-test5.txt="$5"
+file1="$1"
+file2="$2"
+file3="$3"
+file4="$4"
+file5="$5"
 
 # Check if the input files exist
-for file in "$test1.txt" "$test2.txt" "$test3.txt" "$test4.txt" "$test5.txt"; do
+for file in "$file1" "$file2" "$file3" "$file4" "$file5"; do
     if [ ! -f "$file" ]; then
         echo "Error: Input file '$file' not found."
         exit 1
     fi
 done
 
-
 # Use input files provided as arguments
-echo " >> Suffle 500 numbers from 0 to 100000"
-./push_swap $(< "$test1.txt")
+echo " >> Shuffle 500 numbers from 0 to 100000"
+./push_swap $(< "$file1")
 echo " >> with: 2 1 3 6 5 8"
-./push_swap $(< "$test2.txt")
+./push_swap $(< "$file2")
 echo " >> with invalid input: 0 one 2 3 |wc -l"
-./push_swap $(< "$test3.txt") |wc -l
+./push_swap $(< "$file3") | wc -l
 echo " >> with 4 67 3 87 23 but ./push_swap $ARG | ./test/checker_linux $ARG"
-./push_swap $(< "$test4.txt") | ./test/checker_linux $(< "$test4.txt")
+./push_swap $(< "$file4") | ./test/checker_linux $(< "$file4")
 echo " >> with invalid input"
-./push_swap $(< "$test5.txt")
+./push_swap $(< "$file5")
