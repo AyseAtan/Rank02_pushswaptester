6f19ac2c012b5c8119074d6bcb8bd038ff6f0089
Ayse Atan <aatan@c0c2c1.42berlin.de>
2024-02-20 17:21:30 +0100

    Update

--- test.sh
+++ test.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Check if the correct number of arguments is provided
-if [ "$#" -ne 5 ]; then
+if [ "$#" -ne 6 ]; then
     echo "Usage: $0 <test1.txt> <test2.txt> <test3.txt> <test4.txt> <test5.txt> <test6.txt>"
     exit 1
 fi
@@ -24,11 +24,11 @@ done
 
 # Use input files provided as arguments
 echo " >> file 1: Shuffle 500 numbers from -1000 to 100000 |wc -l and memory leaks"
-./push_swap $(< "$file1") |wc -l
-valgrind --leak-check=full --show-leak-kinds=all ./push_swap $(< "$file1") | wc -l
+./push_swap $(< "$file1") | ./test/checker_linux $(< "$file1") |wc -l
+valgrind --leak-check=full ./push_swap $(< "$file1") | wc -l
 echo " >> file 2: Shuffle 500 numbers from -1000 to 100000 mit MAX INT"
-./push_swap $(< "$file2")
-#valgrind --leak-check=full --show-leak-kinds=all ./push_swap $(< "$file2") | wc -l
+./push_swap $(< "$file2") | ./test/checker_linux $(< "$file2")
+valgrind --leak-check=full ./push_swap $(< "$file2") | wc -l
 
 echo " >> file 3: with invalid input: 0 one 2 3 |wc -l"
 ./push_swap $(< "$file3") | wc -l
@@ -36,12 +36,10 @@ echo " >> file 3: with invalid input: 0 one 2 3 |wc -l"
 echo " >> file 4  but with ./push_swap $ARG | ./test/checker_linux $ARG"
 ./push_swap $(< "$file4") | ./test/checker_linux $(< "$file4")
 
-echo " >> with invalid input"
+echo " >> with file 5: invalid input"
 ./push_swap $(< "$file5")
 
-echo " >> file 6:invalid input"
+echo " >> file 6: invalid input"
 ./push_swap $(< "$file6")
 
-echo " >> run comman ./push_swap 0 1 2 3 4 5 6 7 8 9"
-./push_swap 0 1 2 3 4 5 6 7 8 9
 
